9477e0035abad430b090fde0b2033187fda9a24c
yekta <ngyekta@gmail.com>
2025-01-22 23:58:29 +0300

    Remove unused types from schema page

--- src/clean-json/schema.ts
+++ src/clean-json/schema.ts
@@ -1,35 +1,5 @@
 import { z } from "zod";
 
-export type Thesis = {
-  thesis_id: string | null;
-  id_1: string | null;
-  id_2: string | null;
-  name: string | null;
-  year: string | null;
-  title_original: string | null;
-  title_translated: string | null;
-  university: string | null;
-  language: string | null;
-  thesis_type: string | null;
-  subjects_turkish: string[] | null;
-  subjects_english: string[] | null;
-};
-
-export type Extention = {
-  tez_no: number | null;
-  pdf_url: string | null;
-  advisors: string[] | null;
-  pages: number | null;
-  abstract_original: string | null;
-  abstract_translated: string | null;
-  keywords_turkish: string[] | null;
-  keywords_english: string[] | null;
-  status: string | null;
-  institute: string | null;
-  department: string | null;
-  branch: string | null;
-};
-
 export const FinalThesisSchema = z.object({
   // Has to be there
   title_original: z.string().nonempty(),
