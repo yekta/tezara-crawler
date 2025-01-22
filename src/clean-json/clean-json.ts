9b64a4aee8afb82b68dd8e7d8dd2be5d3339ba62
yekta <ngyekta@gmail.com>
2025-01-23 02:24:41 +0300

    Cleaning script improvements

--- src/clean-json/clean-json.ts
+++ src/clean-json/clean-json.ts
@@ -2,6 +2,8 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import fs from "node:fs";
 import { ThesisExtended } from "../types";
+import { FinalThesisSchema } from "./schema";
+import { z } from "zod";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -68,41 +70,80 @@ function processBatch(files: string[], inputDir: string, outputDir: string) {
     const data: ThesisExtended[] = JSON.parse(rawData);
     allTheses.push(...data);
   }
+
+  const cleanedAllTheses: z.infer<typeof FinalThesisSchema>[] = [];
+
   for (let i = 0; i < allTheses.length; i++) {
     const { thesis, problemsCount } = cleanThesis(allTheses[i]);
-    allTheses[i] = thesis;
+    cleanedAllTheses.push(thesis);
     totalProblems += problemsCount;
   }
+
+  const authors = cleanedAllTheses.map((t) => t.author);
+
+  // create an authors.txt if it doesn't exist
+  // in the output directory and write all authors to it
+  // if it does, append the new authors to it
+  const authorsFilePath = path.join(outputDir, "authors.txt");
+  if (!fs.existsSync(authorsFilePath)) {
+    fs.writeFileSync(authorsFilePath, authors.join("\n"));
+  } else {
+    fs.appendFileSync(authorsFilePath, "\n" + authors.join("\n"));
+  }
+
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
   }
+
   console.log(`Processed ${allTheses.length.toLocaleString()} theses.`);
   console.log(`Problem count for the batch: ${totalProblems.toLocaleString()}`);
-  return { fileCount: allTheses.length, totalProblems };
+  return { fileCount: cleanedAllTheses.length, totalProblems };
 }
 
 function cleanThesis(thesis: ThesisExtended) {
   let problemsCount = 0;
   if (thesis.name?.includes("<")) {
     console.log("🟡 Thesis author includes '<'  :", thesis.name);
+    thesis.name = thesis.name.slice(1);
+    thesis.name = thesis.name.trim();
+    console.log("🟢 Thesis author after cleaning:", `"${thesis.name}"`);
     problemsCount++;
   }
+
   const startsWithNumberRegex = /^\d/;
   if (thesis.name && startsWithNumberRegex.test(thesis.name)) {
     console.log("🟡 Thesis author starts with a number:", thesis.name);
+    thesis.name = thesis.name.replace(/^\d+/, "");
+    thesis.name = thesis.name.trim();
+    console.log("🟢 Thesis author after cleaning:", `"${thesis.name}"`);
     problemsCount++;
   }
+
   if (!thesis.name) {
     console.log("🔴 Thesis author is missing:", thesis.name);
     problemsCount++;
   }
+
   if (!thesis.title_original) {
-    console.log(
-      "🔴 Title original is missing:",
-      thesis.title_original,
-      thesis.title_translated,
-      thesis.thesis_id
-    );
+    if (!thesis.title_translated) {
+      console.log(
+        "🔴 Title original and translated are missing:",
+        thesis.thesis_id
+      );
+    } else {
+      console.log(
+        "🟡 Title original is missing:",
+        thesis.title_original,
+        thesis.title_translated,
+        thesis.thesis_id
+      );
+      thesis.title_original = thesis.title_translated;
+      thesis.title_original = thesis.title_original.trim();
+      console.log(
+        "🟢 Title original after cleaning:",
+        `"${thesis.title_original}"`
+      );
+    }
     problemsCount++;
   }
   if (!thesis.university) {
@@ -111,7 +152,10 @@ function cleanThesis(thesis: ThesisExtended) {
   }
   if (thesis.university && thesis.university.startsWith(",")) {
     console.log("🟡 University starts with a comma:", thesis.university);
+    thesis.university = thesis.university.slice(1);
+    thesis.university = thesis.university.trim();
     problemsCount++;
+    console.log("🟢 University after cleaning:", `"${thesis.university}"`);
   }
   if (!thesis.institute) {
     console.log("🔴 Institute is missing:", thesis.institute);
@@ -133,14 +177,20 @@ function cleanThesis(thesis: ThesisExtended) {
       `\nBranch: ${thesis.branch}`,
       `\nThesis ID: ${thesis.thesis_id}`
     ); */
+    thesis.advisors = thesis.advisors
+      .filter((a) => !a.includes("Yer Bilgisi:"))
+      .map((a) => a.trim());
     problemsCount++;
   }
-  if (thesis.advisors && thesis.advisors.some((a) => a.includes("null "))) {
+  if (thesis.advisors && thesis.advisors.some((a) => a.startsWith("null "))) {
     /* console.log("🟡 Advisors include 'null '", thesis.advisors); */
+    thesis.advisors = thesis.advisors.map((a) => a.slice(5));
+    thesis.advisors = thesis.advisors.map((a) => a.trim());
     problemsCount++;
   }
   if (!thesis.advisors || thesis.advisors.length < 1) {
-    console.log("🔴 Advisors is missing:", thesis.thesis_id);
+    /* console.log("🟡 Advisors is missing:", thesis.thesis_id); */
+    thesis.advisors = [];
     problemsCount++;
   }
   if (!thesis.id_1) {
@@ -163,7 +213,51 @@ function cleanThesis(thesis: ThesisExtended) {
     console.log("🔴 Language is missing:", thesis.thesis_id);
     problemsCount++;
   }
-  return { thesis, problemsCount };
+  if (!thesis.title_translated) {
+    thesis.title_translated = null;
+  }
+  if (!thesis.subjects_turkish) {
+    thesis.subjects_turkish = [];
+  }
+  if (!thesis.subjects_english) {
+    thesis.subjects_english = [];
+  }
+  if (!thesis.keywords_turkish) {
+    thesis.keywords_turkish = [];
+  }
+  if (!thesis.keywords_english) {
+    thesis.keywords_english = [];
+  }
+  if (!thesis.pages) {
+    thesis.pages = null;
+  }
+
+  const cleanedThesis = FinalThesisSchema.parse({
+    title_original: thesis.title_original,
+    author: thesis.name,
+    advisors: thesis.advisors,
+    university: thesis.university,
+    institute: thesis.institute,
+    detail_id_1: thesis.id_1,
+    detail_id_2: thesis.id_2,
+    year: thesis.year,
+    thesis_type: thesis.thesis_type,
+    language: thesis.language,
+    subjects_turkish: thesis.subjects_turkish,
+    subjects_english: thesis.subjects_english,
+    keywords_turkish: thesis.keywords_turkish,
+    keywords_english: thesis.keywords_english,
+
+    // Can be null
+    title_translated: thesis.title_translated,
+    abstract_original: thesis.abstract_original,
+    abstract_translated: thesis.abstract_translated,
+    pages: thesis.pages,
+    pdf_url: thesis.pdf_url,
+    department: thesis.department,
+    branch: thesis.department,
+  });
+  return { thesis: cleanedThesis, problemsCount };
 }
 
 if (process.argv[1] === __filename) {
