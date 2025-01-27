0d78f14affd234212a695da8dfd58e8ff4e5360d
yekta <ngyekta@gmail.com>
2025-01-27 07:15:59 +0300

    Remove unused import

--- src/clean-json/clean-json.ts
+++ src/clean-json/clean-json.ts
@@ -1,7 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { z } from "zod";
 import { md5Hash } from "../helpers";
 import { ThesisExtended } from "../types";
 import {
